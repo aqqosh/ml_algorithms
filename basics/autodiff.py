968bbb36a1b602029cb70d08728c3ff1b0026fa6
Elizaveta Koroleva <the.white.nurse@gmail.com>
2021-12-15 17:53:33 +0300

    немного autodiff

---
+++ basics/autodiff.py
@@ -0,0 +1,37 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.python.eager.backprop import GradientTape
+
+# ex 1
+
+x = tf.Variable(3.0)
+
+with tf.GradientTape() as tape:
+    y = x**2
+
+dy_dx = tape.gradient(y, x)
+dy_dx.numpy()
+
+
+# ex 2
+
+w = tf.Variable(tf.random.normal((3, 2)), name="w")
+b = tf.Variable(tf.zeros(2, dtype=tf.float32), name="b")
+x = [[1., 2., 3.]]
+
+with tf.GradientTape() as tape:
+    y = x @ w + b
+    loss = tf.reduce_mean(y**2)
+    
+[dl_w, dl_b] = tape.gradient(loss, [w, b])
+
+# ex 3
+
+layer = tf.keras.layers.Dense(2, activation="relu")
+x = tf.constant([[1., 2., 3.]])
+
+with tf.GradientTape() as tape:
+    y = layer(x)
+    loss = tf.reduce_mean(y**2)
+
+grad = tape.gradient(loss, layer.trainable_variables)
